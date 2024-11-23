1108b3a3ad47e4145ed62750fe85970225b9da0c
cyanghxst <155308268+cyanghxst@users.noreply.github.com>
2024-11-23 11:43:02 -0800

    feat(quick-sort): add QuickSort2.java

---
+++ java/sorting/quick-sort/QuickSort2.java
@@ -0,0 +1,64 @@
+public class QuickSort2 {
+    public static void main(String[] args) {
+        int[] array = generate(10);
+        shuffle(array);
+
+        System.out.printf("Shuffled Array:\n%s\n", java.util.Arrays.toString(array));
+
+        quickSort(array, 0, array.length - 1);
+        System.out.printf("\nSorted Array:\n%s\n", java.util.Arrays.toString(array));
+    }
+
+    public static int[] generate(int size) {
+        int[] array = new  int[size];
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = i + 1;
+        }
+
+        return array;
+    }
+
+    public static void shuffle(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            int random = (int) (Math.random() * array.length);
+            swap(array, i, random);
+        }
+    }
+
+    public static void swap(int[] array, int index1, int index2) {
+        int temp = array[index1];
+        array[index1] = array[index2]; 
+        array[index2] = temp; 
+    }
+
+    public static void quickSort(int[] array, int low, int high) {
+        if (low >= high) return;
+
+        int pivotIndex = partition(array, low, high);
+        quickSort(array, low, pivotIndex - 1); // left side
+        quickSort(array, pivotIndex + 1, high); // right side
+    }
+
+    public static int partition(int[] array, int low, int high) {
+        int i = low - 1;
+        int j = high + 1;
+        int pivot = array[low];
+
+        while (true) {
+            do {
+                i++;
+            } while (array[i] < pivot);
+
+            do {
+                j--;
+            } while (array[j] > pivot);
+
+            if (i > j) break;
+
+            swap(array, i, j);
+        }
+
+        return i;
+    }
+}
