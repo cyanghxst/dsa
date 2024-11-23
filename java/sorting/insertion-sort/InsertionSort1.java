8ac84aa2c6964fe5881c1b9884d17bf5ea5927dc
cyanghxst <155308268+cyanghxst@users.noreply.github.com>
2024-11-22 17:17:35 -0800

    fix(InsertionSort1): correct the insertion functionality

--- java/sorting/insertion-sort/InsertionSort1.java
+++ java/sorting/insertion-sort/InsertionSort1.java
@@ -34,11 +34,11 @@ public class InsertionSort1 {
 
     public static void insertionSort(int[] array) {
         for (int i = 1; i < array.length; i++) {
-            for (int j = i; j >= 0; j--) {
-                if (array[j] < array[i]) {
+            for (int j = i; j > 0; j--) {
+                if (array[j] < array[j - 1]) {
                     int temp = array[j];
-                    array[j] = array[i];
-                    array[i] = temp;
+                    array[j] = array[j - 1];
+                    array[j - 1] = temp;
                 }
             }
         }
