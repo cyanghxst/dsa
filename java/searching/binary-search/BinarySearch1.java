d23fef2e8b68f5e681d3d238082aa039e93b16fe
cyanghxst <155308268+cyanghxst@users.noreply.github.com>
2024-11-22 23:59:36 -0800

    feat(binary-search): add BinarySearch1.java

---
+++ java/searching/binary-search/BinarySearch1.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+public class BinarySearch1 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int[] array = generate(10);
+        Arrays.toString(array);
+
+        System.out.printf("Array:\n%s\n", Arrays.toString(array));
+
+        System.out.println("\nEnter an element to search");
+        System.out.print("\n> ");
+
+        while(!scan.hasNextInt()) {
+            System.out.println("\nInvalid input. Please enter an interger");
+            scan.next();
+        }
+
+        int element = scan.nextInt();
+
+        int index = binarySearch(array, element);
+
+        if (index == -1) {
+            System.out.printf("\n%d not found", element);
+            return;
+        }
+
+        System.out.printf("The index of %d is %d", element, index);
+    }
+
+    public static int[] generate(int size) {
+        int[] array = new int[size];
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int) (Math.random() * 101);
+        }
+
+        return array;
+    }
+}
