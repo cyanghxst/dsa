c6a8495df81450ef32519e6af618607564917efd
cyanghxst <155308268+cyanghxst@users.noreply.github.com>
2024-11-22 17:14:21 -0800

    feat(insertion-sort): add InsertionSort1.java

---
+++ java/sorting/insertion-sort/InsertionSort1.java
@@ -0,0 +1,46 @@
+public class InsertionSort1 {
+    public static void main(String[] args) {
+        int[] array = generate(10);
+        shuffle(array);
+
+        System.out.printf("Shuffled Array:\n%s\n", java.util.Arrays.toString(array));
+
+        insertionSort(array);
+        System.out.printf("\nSorted Array:\n%s\n", java.util.Arrays.toString(array));
+    }
+
+    public static int[] generate(int size) {
+        int[] array = new int[size];
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = i + 1;
+        }
+
+        return array;
+    }
+
+    public static void shuffle(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            int random = (int) (Math.random() * array.length);
+            swap(array, i, random);
+        }
+    }
+
+    public static void swap(int[] array, int index1, int index2) {
+        int temp = array[index1];
+        array[index1] = array[index2];
+        array[index2] = temp;
+    }
+
+    public static void insertionSort(int[] array) {
+        for (int i = 1; i < array.length; i++) {
+            for (int j = i; j >= 0; j--) {
+                if (array[j] < array[i]) {
+                    int temp = array[j];
+                    array[j] = array[i];
+                    array[i] = temp;
+                }
+            }
+        }
+    }
+}
