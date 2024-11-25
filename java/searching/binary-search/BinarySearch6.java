b115b7251d4267c3fb74e2462448ba40e7de7d31
cyanghxst <155308268+cyanghxst@users.noreply.github.com>
2024-11-25 09:15:14 -0800

    feat(binary-search): add BinarySearch6.java

---
+++ java/searching/binary-search/BinarySearch6.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+public class BinarySearch6 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int[] array = generate(10);
+        Arrays.sort(array);
+
+        System.out.printf("Array:\n%s\n", Arrays.toString(array));
+
+        System.out.print("\nEnter an element to search: ");
+        int key = scan.nextInt();
+
+        int index = binarySearch(array, 0, array.length - 1, key);
+
+        if (index == -1) {
+            System.out.printf("\n%d not found\n", key);
+            return;
+        }
+
+        System.out.printf("\nThe index of %d is %d\n", key, index);
+    }
+
+    public static int[] generate(int size) {
+        int[] array = new int[size];
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int) (Math.random() * 101);
+        }
+
+        return array;
+    }
+
+    public static int binarySearch(int[] array, int low, int high, int key) {
+        if (low > high) return -1;
+
+        int middle = (low + high) / 2;
+
+        if (array[middle] == key) return middle;
+
+        return (array[middle] > key)
+            ? binarySearch(array, low, middle - 1, key)
+            : binarySearch(array, middle + 1, high, key);
+    }
+}
