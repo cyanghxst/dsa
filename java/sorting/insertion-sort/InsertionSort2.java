93b216eb319842786286619446ff8f379cdc529c
cyanghxst <155308268+cyanghxst@users.noreply.github.com>
2024-11-25 08:50:33 -0800

    fix(InsertionSort2): correct the swap condition

--- java/sorting/insertion-sort/InsertionSort2.java
+++ java/sorting/insertion-sort/InsertionSort2.java
@@ -24,7 +24,7 @@ public class InsertionSort2 {
     public static void insertionSort(int[] array) {
         for (int i = 1; i < array.length; i++) {
             for (int j = i; j > 0; j--) {
-                if (array[j] > array[j - 1]) {
+                if (array[j] < array[j - 1]) {
                     int temp = array[j];
                     array[j] = array[j - 1];
                     array[j - 1] = temp;
