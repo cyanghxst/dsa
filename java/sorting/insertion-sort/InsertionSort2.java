9065ed58362c319420eab63963d945cda1d8f601
cyanghxst <155308268+cyanghxst@users.noreply.github.com>
2024-11-25 08:49:44 -0800

    feat(insertion-sort): add InsertionSort2.java

---
+++ java/sorting/insertion-sort/InsertionSort2.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class InsertionSort2 {
+    public static void main(String[] args) {
+        int[] array = generate(10);
+
+        System.out.printf("Array:\n%s\n", Arrays.toString(array));
+
+        insertionSort(array);
+
+        System.out.printf("\nArray:\n%s\n", Arrays.toString(array));
+    }
+
+    public static int[] generate(int size) {
+        int[] array = new int[size];
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int) (Math.random() * 101);
+        }
+
+        return array;
+    }
+
+    public static void insertionSort(int[] array) {
+        for (int i = 1; i < array.length; i++) {
+            for (int j = i; j > 0; j--) {
+                if (array[j] > array[j - 1]) {
+                    int temp = array[j];
+                    array[j] = array[j - 1];
+                    array[j - 1] = temp;
+                }
+            }
+        }
+    }
+}
