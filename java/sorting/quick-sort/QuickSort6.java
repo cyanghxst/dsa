fa4394806940a367a1f4ce27bb67b8a4c35b7b37
cyanghxst <155308268+cyanghxst@users.noreply.github.com>
2024-11-25 22:50:19 -0800

    feat(quick-sort): add QuickSort6.java

---
+++ java/sorting/quick-sort/QuickSort6.java
@@ -0,0 +1,70 @@
+import java.util.*;
+
+public class QuickSort6 {
+    public static void main(String[] args) {
+        int[] array = generate(10);
+        System.out.printf("Array:\n%s\n", Arrays.toString(array));
+
+        quickSort(array, 0, array.length - 1);
+
+        System.out.printf("\nSorted Array:\n%s\n", Arrays.toString(array));
+    }
+
+    public static int[] generate(int size) {
+        int[] array = new int[size];
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int) (Math.random() * 101);
+        }
+
+        return array;
+    }
+
+    public static void quickSort(int[] array, int low, int high) {
+        if (low >= high) return;
+
+        int partitionIndex = partition(array, low, high);
+
+        quickSort(array, low, partitionIndex);
+        quickSort(array, partitionIndex + 1, high);
+    }
+
+    public static int partition(int[] array, int low, int high) {
+        int pivot = array[low];
+
+        int i = low - 1;
+        int j = high + 1;
+
+        while (true) {
+            do {
+                i++;
+            } while (array[i] < pivot);
+
+            do {
+                j++;
+            } while (array[j] > pivot);
+
+            if (i > j) return i;
+
+            swap(array, i, j);
+        }
+    }
+
+    public static int medianOfThree(int[] array, int a, int b, int c) {
+        if (array[a] > array[b] && array[a] < array[c] || array[a] < array[b] && array[a] > array[c]) {
+            return a;
+        }
+
+        if (array[b] > array[a] && array[b] < array[c] || array[b] < array[a] && array[b] > array[c]) {
+            return b;
+        }
+
+        return c;
+    }
+
+    public static void swap(int[] array, int left, int right) {
+        int temp = array[left];
+        array[left] = array[right];
+        array[right] = temp;
+    }
+}
