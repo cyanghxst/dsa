8c7470e12857fdc89be6344e7eed5d883e408614
cyanghxst <155308268+cyanghxst@users.noreply.github.com>
2024-11-26 21:27:11 -0800

    feat(quick-select): add QuickSelect2.java

---
+++ java/selecting/quick-select/QuickSelect2.java
@@ -0,0 +1,57 @@
+import java.util.*;
+
+public class QuickSelect2 {
+    public static void main(String[] args) {
+        int[] array = generate(10, false);
+        System.out.printf("Array:\n%s\n", Arrays.toString(array));
+
+        int key = 4;
+        int element = quickSelect(array, 0, array.length - 1, key);
+
+        System.out.printf("\nThe %dth smallest element is %d\n", key, element);
+    }
+    
+    public static int[] generate(int size, boolean fixedSeed) {
+        Random random = fixedSeed ? new Random(420) : new Random();
+        int[] array = new int[size];
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextInt(101);
+        }
+
+        return array;
+    }
+
+    public static int quickSelect(int[] array, int low, int high, int key) {
+        int pivotIndex = partition(array, low, high);
+
+        if (pivotIndex == key - 1) return array[pivotIndex];
+
+        return (pivotIndex > key - 1)
+            ? partition(array, low, pivotIndex - 1)
+            : partition(array, pivotIndex + 1, high);
+    }
+
+    public static int partition(int[] array, int low, int high) {
+        int pivot = array[high];
+
+        int i = low - 1;
+
+        for (int j = low; j <= high; j++) {
+            if (array[j] <= pivot) {
+                i++;
+                if (j > 1) {
+                    swap(array, j, i);
+                }
+            }
+        }
+
+        return i;
+    }
+
+    public static void swap(int[] array, int i, int j) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
